bbccc3c4a6f2ceb775a98b424c7e1ba70ea5a748
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-02-27 14:44:22 -0800

    styling fix

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,10 @@ const Hero = () => {
         <div className='hero-content-container'>
             
             <div className='hero-text'>
-            <motion.div whileInView={{opacity: '100%'}} initial={{opacity: "0"}} transition={{duration: 2, ease: "easeInOut"}}>
+            <motion.div 
+              whileInView={{opacity: '100%'}} 
+              initial={{opacity: "0"}} 
+              transition={{duration: 2, ease: "easeInOut"}}>
             <h1>REDISCOVER,<br></br> REIMAGINE,<br></br>RELIVE</h1>
             <p className='hero-p'>There are a number of reasons you may need a block of text and when you do, a random paragraph can be the perfect solution. If you happen to be a web designer and you need some random text to show in your layout, a random paragraph can be an excellent way to do this.</p>
             <Link to="/about" style={{textDecoration: "none", color: "#fff"}} ><p className='cta-1'>Learn More</p></Link>
